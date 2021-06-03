3a2194b1fd1e5c0b2b23a80470a2f484a942b3a5
joseju13 <test@fake.com>
2021-06-03 15:45:39 -0500

    publicando archivos

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+/*Downloaded from https://www.codeseek.co/AlvaroFelipe/aumentar-y-disminuir-tamaandxf1o-de-fuente-con-javascript-wjvwww */
+var getFontSize = function getFontSize() {
+    return parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--font-size'));
+  };
+  
+  var fontUp = function fontUp(element) {
+    element.addEventListener('click', function () {
+      var fontSize = getFontSize();
+      document.documentElement.style.setProperty('--font-size', '' + fontSize * 1.1);
+    });
+  };
+  
+  var fontDown = function fontDown(element) {
+    element.addEventListener('click', function () {
+      var fontSize = getFontSize();
+      document.documentElement.style.setProperty('--font-size', '' + fontSize * 0.9);
+    });
+  };
+  
+  fontUp(document.getElementById('font-up'));
+  fontDown(document.getElementById('font-down'));

7d7721d271892593abfa6a861690dac19c492ac7
joseju13 <test@fake.com>
2021-06-04 21:35:54 -0500

    Filtro blanco y negro

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 
+//Botones para agrandar y disminuir el tamaño de la fuente
 var zoom=1;
- 
+
 document.getElementById("font-up").addEventListener("click",function(e){
 if(zoom<=1.55){
     zoom+=.30;
@@ -18,5 +19,27 @@ function resize() {
     document.getElementById("text").style.zoom=zoom;
     document.getElementById("value").innerHTML="x"+zoom.toFixed(2);
 }
+
+//Cambiar a filtro blanco y negro
+
+
+  document.getElementById("btn-check-2-outlined").addEventListener("click",function(e){
+
+
+
+    if (document.getElementById('btn-check-2-outlined').checked){
+      document.getElementById("imagen-A").style.filter = "grayscale(100%)"
+      document.getElementById("cuerpo").style.filter = "grayscale(100%)"
+      
+    } else{
+      document.getElementById("imagen-A").style.filter = "none"
+      document.getElementById("cuerpo").style.filter = "none"
+    }
+
+    
+
+
+});
+
 
 

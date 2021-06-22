32f5dd706513a168034af66e052c0566767ad9a2
joseju13 <test@fake.com>
2021-06-22 11:52:18 -0500

    Cambios

--- Static/accesibilidad.js
+++ Static/accesibilidad.js
@@ -1,119 +1,40 @@
-let contador_1 = 1;
-let contador_2;
-// document.getElementById("btn-byno").addEventListener("click",function(e){
-
-
-
-//   if (document.getElementById('btn-byno').checked){
-   
-//     blancoNegro();
-
-//   } else{
-//     noByN();
-//   }
-// });
-
-
-
-
-// $(document).ready(function(){
-  
-//     $("#small").click(function(event){
-//       event.preventDefault();
-//       $("h1").animate({"font-size":"24px"});
-//       $("h2").animate({"font-size":"16px"});
-//       $("p").animate({"font-size":"12px", "line-height":"16px"});
-//       console.log("Funciona")
-      
-//     });
-    
-//     $("#medium").click(function(event){
-//       event.preventDefault();
-//       $("h1").animate({"font-size":"36px"});
-//       $("h2").animate({"font-size":"24px"});
-//       $("p").animate({"font-size":"14px", "line-height":"20px"});
-      
-//     });
-    
-//     $("#large").click(function(event){
-//       event.preventDefault();
-//       $("h1").animate({"font-size":"48px"});
-//       $("h2").animate({"font-size":"30px"});
-//       $("p").animate({"font-size":"16px", "line-height":"20px"});
-      
-//     });
-    
-//     $("#max").click(function(event){
-//       event.preventDefault();
-//       $("h1").animate({"font-size":"56px"});
-//       $("h2").animate({"font-size":"42px"});
-//       $("p").animate({"font-size":"24px", "line-height":"26px"});
-      
-//     });
-  
-//     $( "a" ).click(function() {
-//      $("a").removeClass("selected");
-//     $(this).addClass("selected");
-    
-//    });
-  
-//   });
-
-
-
-
-
-
 //  Blanco y negro
- document.getElementById("btn-byno").addEventListener("click", (e) => {
-   contador_1 += 1;
-   contador_2 = contador_1 % 2;
-   if (contador_2 == 0) {
-     blancoNegro();
-   } else {
-     noByN();
-     contador_1 = 1;
-   }
- });
-
-// Resaltar Links
-document.getElementById("btn-resalt").addEventListener("click", (e) => {
-  contador_1 += 1;
-  contador_2 = contador_1 % 2;
-  if (contador_2 == 0) {
-    underline();
+document.getElementById("btn-byno").addEventListener("click", function (e) {
+  if (document.getElementById("btn-byno").checked) {
+    blancoNegro();
   } else {
-    noUnderline();
-    contador_1 = 1;
+    noByN();
   }
 });
 
+// Resaltar Links
+document.getElementById("btn-resalt").addEventListener("click", function (e) {
+  if (document.getElementById("btn-resalt").checked) {
+    underline();
+  } else {
+    noUnderline();
+  } 
+});
+
 // Contraste alto
-document.getElementById("btn-contra").addEventListener("click", (e) => {
-  contador_1 += 1;
-  contador_2 = contador_1 % 2;
-  if (contador_2 == 0) {
+document.getElementById("btn-contra").addEventListener("click", function (e) {
+  if (document.getElementById("btn-contra").checked) {
     contrast();
   } else {
     noContrast();
-    contador_1 = 1;
   }
 });
 
 // Fuente Legible
-document.getElementById("btn-legible").addEventListener("click", (e) => {
-  contador_1 += 1;
-  contador_2 = contador_1 % 2;
-  if (contador_2 == 0) {
+document.getElementById("btn-legible").addEventListener("click", function (e) {
+  if (document.getElementById("btn-legible").checked) {
     legible();
   } else {
     noLegible();
-    contador_1 = 1;
   }
 });
 
 //Restablecer cambios
-
 document.getElementById("btn-reload").addEventListener("click", (e) => {
   noByN();
   noUnderline();
@@ -122,11 +43,11 @@ document.getElementById("btn-reload").addEventListener("click", (e) => {
 });
 
 // Blanco y negro a toda la pagina completa
-
 let black_white = document.getElementsByTagName("html");
 function blancoNegro() {
+  var checkbox = document.getElementById("btn-byno");
+  localStorage.setItem("btn-byno", checkbox.checked);
   for (let i = 0; i < black_white.length; i++) {
-
     black_white[i].style.filter = "grayscale(100%)";
   }
 }
@@ -154,43 +75,47 @@ function noUnderline() {
 let high_contrast = document.getElementsByTagName("a");
 
 function contrast() {
-  $( "div" ).css( "color", "white" );
-  $( "span" ).css( "color", "white" );
-  $( "p" ).css( "color", "white" );
-  $( ".flat-event" ).css( "background", "#0D1117" );
-  $( ".wrap-quote " ).css( "background", "#0D1117" );
-  $( ".latest-blog .post-bg" ).css( "background", "#2B3240" );
-  $( ".card" ).css( "background", "#0D1117" );
-
-
+  $("div").css("color", "white");
+  $("span").css("color", "white");
+  $("p").css("color", "white");
+  $(".flat-event").css("background", "#0D1117");
+  $(".wrap-quote ").css("background", "#0D1117");
+  $(".latest-blog .post-bg").css("background", "#2B3240");
+  $(".card").css("background", "#0D1117");
 
   document.body.style.backgroundColor = "#0D1117";
   for (let i = 0; i < high_contrast.length; i++) {
     high_contrast[i].style.color = "#66D1D1";
-      $(high_contrast[i]).hover(function(){
+    $(high_contrast[i]).hover(
+      function () {
         $(this).css("color", "yellow");
-        }, function(){
+      },
+      function () {
         $(this).css("color", "#66D1D1");
-      });
+      }
+    );
   }
 }
 function noContrast() {
   document.body.style.backgroundColor = "#fff";
-  $( "div" ).css( "color", "" );
-  $( "span" ).css( "color", "" );
-  $( "p" ).css( "color", "" );
-  $( ".flat-event" ).css( "background", "inherit" );
-  $( ".wrap-quote " ).css( "background", "inherit" );
-  $( ".latest-blog .post-bg" ).css( "background", "" );
-  $( ".card" ).css( "background", "" );
-  
+  $("div").css("color", "");
+  $("span").css("color", "");
+  $("p").css("color", "");
+  $(".flat-event").css("background", "inherit");
+  $(".wrap-quote ").css("background", "inherit");
+  $(".latest-blog .post-bg").css("background", "");
+  $(".card").css("background", "");
+
   for (let i = 0; i < high_contrast.length; i++) {
     high_contrast[i].style.color = "";
-    $(high_contrast[i]).hover(function(){
-      $(this).css("color", "");
-      }, function(){
-      $(this).css("color", "");
-    });
+    $(high_contrast[i]).hover(
+      function () {
+        $(this).css("color", "");
+      },
+      function () {
+        $(this).css("color", "");
+      }
+    );
   }
 }
 
@@ -199,21 +124,19 @@ let legible_font = document.getElementsByTagName("p");
 let legible_font_2 = document.getElementsByTagName("span");
 
 function legible() {
-for (let i = 0; i < legible_font.length; i++ ){
-  legible_font[i].style.fontWeight = "bold";
-}
+  for (let i = 0; i < legible_font.length; i++) {
+    legible_font[i].style.fontWeight = "bold";
+  }
 
-for (let i = 0; i < legible_font_2.length; i++){
-  legible_font_2[i].style.fontWeight = "bold";
-}
-
+  for (let i = 0; i < legible_font_2.length; i++) {
+    legible_font_2[i].style.fontWeight = "bold";
+  }
 }
 function noLegible() {
-  for (let i = 0; i < legible_font.length; i++ ){
+  for (let i = 0; i < legible_font.length; i++) {
     legible_font[i].style.fontWeight = "normal";
   }
-  for (let i = 0; i < legible_font_2.length; i++){
+  for (let i = 0; i < legible_font_2.length; i++) {
     legible_font_2[i].style.fontWeight = "normal";
   }
-
 }
--- Static/chat-bot.js
+++ Static/chat-bot.js
@@ -1,220 +1,216 @@
+// Chat Collapsible
+var coll = document.getElementsByClassName("collapsible_down");
+var element = document.getElementById("chat-button");
+var contentBig = document.getElementsByClassName("chat-bar-collapsible ");
 
-    // Chat Collapsible
-    var coll = document.getElementsByClassName("collapsible_down");
-    var element = document.getElementById("chat-button");
-    var contentBig = document.getElementsByClassName("chat-bar-collapsible ");
-    
-    // Ocultar y mostrar elementos
-    document.getElementById("oculAsis").style.display = "none";
-    setTimeout(() => {
-        document.getElementById("bubble").style.display = "none";
-    }, 1000);
-    setTimeout(() => {
-        document.getElementById("bubble").style.display = "block";
-    }, 5000);
-    setTimeout(() => {
-        document.getElementById("bubble").style.display = "none";
-    }, 10000);
+// Ocultar y mostrar elementos
+document.getElementById("oculAsis").style.display = "none";
 
-    
-    
-    // Comportamiento de la ventana al momento de abrir y cerrar
-    $(document).ready(function () {
-        if (element.clientWidth < 100) {
-            $(contentBig).css("box-shadow", "none");
-        }
-    });
-    for (let i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            var content = this.nextElementSibling;
-    
-            if (content.style.maxHeight) {
-                // Cuando la ventana esta cerrada
-                content.style.maxHeight = null;
-                element.className = "collapsible_down";
-                $(contentBig).css("box-shadow", "none");
-                document.getElementById("oculAsis").style.display = "none";
-                // document.getElementById("bubble").style.display = "block";
-                // setTimeout(() => {
-                //     document.getElementById("bubble").style.display = "block";
-                // }, 7000);
-                // setTimeout(() => {
-                //     document.getElementById("bubble").style.display = "none";
-                // }, 40000);
-            } else {
-                // Cuando la ventana esta abierta
-    
-                content.style.maxHeight = content.scrollHeight + "px";
-                element.className = "collapsible_up";
-                $(contentBig).css("box-shadow", "0 8px 16px 0 rgba(0, 0, 0, 0.2)");
-                document.getElementById("oculAsis").style.display = "block";
-                document.getElementById("bubble").style.display = "none";
-            }
-        });
+setTimeout(function() {document.getElementById("bubble").style.display = "none";},10000)
+
+// Comportamiento de la ventana al momento de abrir y cerrar
+$(document).ready(function () {
+  if (element.clientWidth < 100) {
+    $(contentBig).css("box-shadow", "none");
+  }
+});
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    var content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      // Cuando la ventana esta cerrada
+      content.style.maxHeight = null;
+      element.className = "collapsible_down";
+      $(contentBig).css("box-shadow", "none");
+      document.getElementById("oculAsis").style.display = "none";
+      document.getElementById("bubble").style.display = "none";
+      document.getElementById("chat-icon").style.display = "block";
+      
+    } else {
+      // Cuando la ventana esta abierta
+
+      content.style.maxHeight = content.scrollHeight + "px";
+      element.className = "collapsible_up";
+      $(contentBig).css("box-shadow", "0 8px 16px 0 rgba(0, 0, 0, 0.2)");
+      document.getElementById("oculAsis").style.display = "block";
+      document.getElementById("bubble").style.display = "none";
+      document.getElementById("chat-icon").style.display = "none";
     }
-    
-    //Obtener el tiempo y plasmarlo
-    function getTime() {
-        let today = new Date();
-        hours = today.getHours();
-        minutes = today.getMinutes();
-    
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
-    
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-    
-        let time = hours + ":" + minutes;
-        return time;
-    }
-    
-    // Obtener el primer mensaje
-    function firstBotMessage() {
-        let firstMessage =
-            "Bienvenido, soy la asistente virtual <b> Wendy </b>  <br>A continuacion puede seleccionar una respuesta o escribirla ";
-        document.getElementById("botStarterMessage").innerHTML =
-            '<p class="botText"><span>' + firstMessage + "</span></p>";
-    
-        let time = getTime();
-    
-        $("#chat-timestamp").append(time);
-        document.getElementById("userInput").scrollIntoView(false);
-    }
-    
-    firstBotMessage();
-    
-    // Obtener respuestas
-    function getHardResponse(userText) {
-        let botResponse = getBotResponse(userText);
-        let botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
-        $("#chatbox").append(botHtml);
-    
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    }
-    
-    //Obtener el texto del recuadro para ingresar datoa y procesarlo
-    function getResponse() {
-        let userText = $("#textInput").val();
-    
-        if (userText == "") {
-            userText = document.getElementById("examp").value;
-        }
-        let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
-    
-        $("#textInput").val("");
-        $("#chatbox").append(userHtml);
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    
-        setTimeout(() => {
-            getHardResponse(userText);
-        }, 1000);
-    }
-    
-    // Handles sending text via button clicks
-    function buttonSendText(sampleText) {
-        let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
-    
-        $("#textInput").val("");
-        $("#chatbox").append(userHtml);
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    }
-    
-    function sendButton() {
-        getResponse();
-    }
-    
-    function estadia() {
-        let userText = $("#textInput").val();
-    
-        if (userText == "") {
-            userText = document.getElementById("estadia").value;
-        }
-        let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
-    
-        $("#textInput").val("");
-        $("#chatbox").append(userHtml);
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    
-        setTimeout(() => {
-            getHardResponse(userText);
-        }, 1000);
-    }
-    
-    function estancia() {
-        let userText = $("#textInput").val();
-    
-        if (userText == "") {
-            userText = document.getElementById("estancia").value;
-        }
-        let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
-    
-        $("#textInput").val("");
-        $("#chatbox").append(userHtml);
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    
-        setTimeout(() => {
-            getHardResponse(userText);
-        }, 1000);
-    }
-    
-    function reinscripcion() {
-        let userText = $("#textInput").val();
-    
-        if (userText == "") {
-            userText = document.getElementById("reinscripcion").value;
-        }
-        let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
-    
-        $("#textInput").val("");
-        $("#chatbox").append(userHtml);
-        document.getElementById("chat-bar-bottom").scrollIntoView(true);
-    
-        setTimeout(() => {
-            getHardResponse(userText);
-        }, 1000);
-    }
-    
-    // Press enter to send a message
-    $("#textInput").keypress(function (e) {
-        if (e.which == 13) {
-            getResponse();
-        }
-    });
-    
-    function getBotResponse(input) {
-        if (input == "hola" || input == "Info") {
-            return "¿Qué deseas hacer?";
-        }
-    
-        if (
-            input == "Proceso de estadia" ||
-            input == "proceso de estadía" ||
-            input == "estadia" ||
-            input == "estadía"
-        ) {
-            return "Deberás presentar tus papeles a las oficinas y cargar tus documentos.";
-        }
-        if (
-            input == "Proceso de estancia" ||
-            input == "proceso de estancia" ||
-            input == "estancia" ||
-            input == "estancias"
-        ) {
-            return "Deberás subir tus documentos en orden en las fechas establecidas ";
-        }
-        if (
-            input == "Proceso de reinscripcion" ||
-            input == "proceso de reinscripción" ||
-            input == "reinscripcion" ||
-            input == "reinscripción"
-        ) {
-            return "Proceso de reiscripcion:  ";
-        } else {
-            return "Opcion incorrecta, intente nuevamente";
-        }
-    }
-    
-    
+  });
+}
+
+//Obtener el tiempo y plasmarlo
+function getTime() {
+  let today = new Date();
+  hours = today.getHours();
+  minutes = today.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  let time = hours + ":" + minutes;
+  return time;
+}
+
+// Obtener el primer mensaje
+function firstBotMessage() {
+  let firstMessage =
+    "Bienvenido, soy la asistente virtual <b> Wendy </b>  <br>A continuacion puede seleccionar una respuesta o escribirla ";
+  document.getElementById("botStarterMessage").innerHTML =
+    '<p class="botText"><span>' + firstMessage + "</span></p>";
+
+  let time = getTime();
+
+  $("#chat-timestamp").append(time);
+
+  document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
+
+// Obtener respuestas
+function getHardResponse(userText) {
+  let botResponse = getBotResponse(userText);
+  let botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
+  $("#chatbox").append(botHtml);
+
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+//Obtener el texto del recuadro para ingresar datoa y procesarlo
+function getResponse() {
+  let userText = $("#textInput").val();
+
+  if (userText == "") {
+    userText = document.getElementById("examp").value;
+  }
+  let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
+}
+
+// Handles sending text via button clicks
+function buttonSendText(sampleText) {
+  let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+function sendButton() {
+  getResponse();
+}
+
+function estadia() {
+  let userText = $("#textInput").val();
+
+  if (userText == "") {
+    userText = document.getElementById("estadia").value;
+  }
+  let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
+  
+}
+
+function estancia() {
+  let userText = $("#textInput").val();
+
+  if (userText == "") {
+    userText = document.getElementById("estancia").value;
+  }
+  let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
+}
+
+function reinscripcion() {
+  let userText = $("#textInput").val();
+
+  if (userText == "") {
+    userText = document.getElementById("reinscripcion").value;
+  }
+  let userHtml = `<p class="userText"><span>  ${userText} </span></p>`;
+
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
+}
+
+// Press enter to send a message
+$("#textInput").keypress(function (e) {
+  if (e.which == 13) {
+    getResponse();
+  }
+});
+
+function getBotResponse(input) {
+  if (input == "hola" || input == "Info") {
+    return "¿Qué deseas hacer?";
+  }
+
+  if (
+    input == "Proceso de estadia" ||
+    input == "proceso de estadía" ||
+    input == "Proceso de estadía" ||
+    input == "estadia" ||
+    input == "estadía"
+  ) {
+    return (
+      "Para iniciar el trámite el alumno debe: <br> Realizar el registro y subir los siguientes documentos al sistema SIGA" +
+      `<a href="#">(AQUÍ)</a>` +
+      " <br> 1.-Documento de Solicitud de Estadía <br> 2.-Carta de Exclusión de Responsabilidad Covid, para la realización de Estadía <br> 3.-Identificación con fotografía del padre y/o tutor legal <br> 4.-Constancia de vigencia del IMSS. <br> 5.-Carta de Exclusión de Responsabilidad (DVEU-RG.16) o carta de exclusión de responsabilidad  (DVEU-RG.17) si el alumno es su propio tutor. Con los soportes de identificación con fotografía del tutor legal. <br>" +
+      `<a href="#">Presiona aquí para mas informacion</a>`
+    );
+  }
+  if (
+    input == "Proceso de estancia" ||
+    input == "proceso de estancia" ||
+    input == "estancia" ||
+    input == "estancias"
+  ) {
+    return "Para iniciar el trámite el alumno debe: <br> Realizar el registro y subir los siguientes documentos al sistema SIGA" +
+    `<a href="#">(AQUÍ)</a>` +
+    " <br> 1.-Documento de Solicitud de Estancia <br> 2.-Carta de Exclusión de Responsabilidad Covid, para la realización de Estadía <br> 3.-Identificación con fotografía del padre y/o tutor legal <br> 4.-Constancia de vigencia del IMSS. <br> 5.-Carta de Exclusión de Responsabilidad (DVEU-RG.16) o carta de exclusión de responsabilidad  (DVEU-RG.17) si el alumno es su propio tutor. Con los soportes de identificación con fotografía del tutor legal." +
+    `<a href="#">Presiona aquí para mas informacion</a>`;
+  }
+  if (
+    input == "Proceso de reinscripcion" ||
+    input == "proceso de reinscripción" ||
+    input == "reinscripcion" ||
+    input == "reinscripción"
+  ) {
+    return "Proceso de reiscripcion:  ";
+  } else {
+    return "Opcion incorrecta, intente nuevamente";
+  }
+}
